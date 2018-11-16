e966a121447b20f3e39aadf5ab7b136e4688672c
ezwelapiksw <ezwelapiksw@gmail.com>
2018-11-16 20:12:15 +0900

    ~

--- ezwel_project/ezwel_if/src/main/java/com/ezwel/htl/interfaces/service/dto/agentJob/AgentJobInDTO.java
+++ ezwel_project/ezwel_if/src/main/java/com/ezwel/htl/interfaces/service/dto/agentJob/AgentJobInDTO.java
@@ -1,6 +1,7 @@
 package com.ezwel.htl.interfaces.service.dto.agentJob;
 
 import com.ezwel.htl.interfaces.commons.abstracts.AbstractEntity;
+import com.ezwel.htl.interfaces.commons.annotation.APIFields;
 
 /**
  * <pre>
@@ -11,8 +12,13 @@ import com.ezwel.htl.interfaces.commons.abstracts.AbstractEntity;
  */
 public class AgentJobInDTO extends AbstractEntity {
 
+	@APIFields(description = "주문대사(제휴사) Input 주문번호", required=true, maxLength=100)
 	private String rsvNo;
+	
+	@APIFields(description = "주문대사(제휴사) Input 주문시작일", required=true, maxLength=8)
 	private String rsvDateStart;
+	
+	@APIFields(description = "주문대사(제휴사) Input 주문종료일", required=true, maxLength=8)
 	private String rsvDateEnd;
 
 	public String getRsvNo() {
