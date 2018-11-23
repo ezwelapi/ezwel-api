c3f1f0b0540c58e220a7f5deb768954a64970e5f
ezwelapiksw <ezwelapiksw@gmail.com>
2018-11-23 16:38:33 +0900

    ~

--- ezwel_project/ezwel_if_server/src/main/java/com/ezwel/htl/interfaces/server/controller/OutsideController.java
+++ ezwel_project/ezwel_if_server/src/main/java/com/ezwel/htl/interfaces/server/controller/OutsideController.java
@@ -1,5 +1,8 @@
 package com.ezwel.htl.interfaces.server.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -8,7 +11,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -17,16 +19,10 @@ import com.ezwel.htl.interfaces.commons.exception.APIException;
 import com.ezwel.htl.interfaces.commons.http.data.UserAgentSDO;
 import com.ezwel.htl.interfaces.server.commons.spring.LApplicationContext;
 import com.ezwel.htl.interfaces.server.service.OutsideService;
-import com.ezwel.htl.interfaces.service.data.agentJob.AgentJobInSDO;
-import com.ezwel.htl.interfaces.service.data.agentJob.AgentJobOutSDO;
-import com.ezwel.htl.interfaces.service.data.record.RecordInSDO;
-import com.ezwel.htl.interfaces.service.data.record.RecordOutSDO;
-import com.ezwel.htl.interfaces.service.data.saleStop.SaleStopInSDO;
-import com.ezwel.htl.interfaces.service.data.saleStop.SaleStopOutSDO;
-import com.ezwel.htl.interfaces.service.data.view.ViewInSDO;
-import com.ezwel.htl.interfaces.service.data.view.ViewOutSDO;
-import com.ezwel.htl.interfaces.service.data.voucherReg.VoucherRegInSDO;
-import com.ezwel.htl.interfaces.service.data.voucherReg.VoucherRegOutSDO;
+import com.ezwel.htl.interfaces.service.data.allReg.AllRegOutSDO;
+import com.ezwel.htl.interfaces.service.data.faclSearch.FaclSearchInSDO;
+import com.ezwel.htl.interfaces.service.data.faclSearch.FaclSearchOutSDO;
+import com.ezwel.htl.interfaces.service.data.sddSearch.SddSearchOutSDO;
 
 /**
  * <pre>
@@ -61,14 +57,14 @@ public class OutsideController {
 	@ResponseBody
 	@APIOperation(description="전체시설일괄등록 인터페이스")
 	@RequestMapping(value="/service/allReg")
-	public ResponseEntity<RecordOutSDO> callAllReg(@PathVariable("httpAgentId") String httpAgentId, UserAgentSDO userAgentDTO, HttpServletRequest request, HttpServletResponse response) {
-		logger.debug("[START] callAllReg {}", recordInSDO);
+	public ResponseEntity<AllRegOutSDO> callAllReg(UserAgentSDO userAgentSDO, HttpServletRequest request, HttpServletResponse response) {
+		logger.debug("[START] callAllReg {}", userAgentSDO);
 		
-		ResponseEntity<RecordOutSDO> out = null;
-		RecordOutSDO serviceOut = null;
+		ResponseEntity<AllRegOutSDO> out = null;
+		AllRegOutSDO serviceOut = null;
 
 		try {
-			if(recordInSDO == null) {
+			if(userAgentSDO == null) {
 				throw new APIException("입력값이 존재하지 않습니다.");
 			}
 			
@@ -79,16 +75,16 @@ public class OutsideController {
 			 */
 			
 			//Advice & Interceptor 최적화후 작업 추가 진행
-			serviceOut = outsideService.callAllReg(recordInSDO);
+			serviceOut = outsideService.callAllReg(userAgentSDO);
 
-			out = new ResponseEntity<RecordOutSDO>(serviceOut, HttpStatus.OK);
+			out = new ResponseEntity<AllRegOutSDO>(serviceOut, HttpStatus.OK);
 		}
 		catch(Exception e) {
-			serviceOut = new RecordOutSDO(); 
+			serviceOut = new AllRegOutSDO(); 
 			/**
 			 * 장애 발생시 code, message 세팅 
 			 */
-			out = new ResponseEntity<RecordOutSDO>(serviceOut, HttpStatus.OK);
+			out = new ResponseEntity<AllRegOutSDO>(serviceOut, HttpStatus.OK);
 			e.printStackTrace();
 		}
 		
@@ -97,125 +93,69 @@ public class OutsideController {
 	}
 	
 	@ResponseBody
-	@APIOperation(description="시설판매중지설정 인터페이스")
-	@RequestMapping(value="/{httpAgentId}/facl/saleStop")
-	public ResponseEntity<SaleStopOutSDO> callSaleStop(@PathVariable("httpAgentId") String httpAgentId, SaleStopInSDO saleStopInSDO, HttpServletRequest request, HttpServletResponse response) {
-		logger.debug("[START] callSaleStop {}", saleStopInSDO);
+	@APIOperation(description="시설검색 인터페이스")
+	@RequestMapping(value="/service/callFaclSearch")
+	public ResponseEntity<List<FaclSearchOutSDO>> callFaclSearch(UserAgentSDO userAgentDTO, FaclSearchInSDO faclSearchDTO, HttpServletRequest request, HttpServletResponse response) {
+		logger.debug("[START] callFaclSearch {} {}", userAgentDTO, faclSearchDTO);
 		
-		ResponseEntity<SaleStopOutSDO> out = null;
-		SaleStopOutSDO serviceOut = null;
-
+		ResponseEntity<List<FaclSearchOutSDO>> out = null;
+		List<FaclSearchOutSDO> serviceOut = null;
+		FaclSearchOutSDO faclSearchOut = null; 
 		try {
-			if(saleStopInSDO == null) {
+			if(userAgentDTO == null || faclSearchDTO == null) {
 				throw new APIException("입력값이 존재하지 않습니다.");
 			}
 			
 			//Advice & Interceptor 최적화후 작업 추가 진행
-			serviceOut = outsideService.callSaleStop(saleStopInSDO);
+			serviceOut = outsideService.callFaclSearch(userAgentDTO, faclSearchDTO);
 
-			out = new ResponseEntity<SaleStopOutSDO>(serviceOut, HttpStatus.OK);
+			out = new ResponseEntity<List<FaclSearchOutSDO>>(serviceOut, HttpStatus.OK);
 		}
 		catch(Exception e) {
-			serviceOut = new SaleStopOutSDO(); 
-		
-			out = new ResponseEntity<SaleStopOutSDO>(serviceOut, HttpStatus.OK);
+			serviceOut = new ArrayList<FaclSearchOutSDO>(); 
+			faclSearchOut = new FaclSearchOutSDO();
+			
+			serviceOut.add(faclSearchOut);
+			out = new ResponseEntity<List<FaclSearchOutSDO>>(serviceOut, HttpStatus.OK);
 			e.printStackTrace();
 		}
 		
-		logger.debug("[END] callSaleStop {}", out);
+		logger.debug("[END] callFaclSearch {}", out);
 		return out;
 	}
 	
 
 	@ResponseBody
-	@APIOperation(description="예약내역조회 인터페이스")
-	@RequestMapping(value="/{httpAgentId}/rsv/view")
-	public ResponseEntity<ViewOutSDO> callView(@PathVariable("httpAgentId") String httpAgentId, ViewInSDO viewInSDO, HttpServletRequest request, HttpServletResponse response) {
-		logger.debug("[START] callView {}", viewInSDO);
+	@APIOperation(description="당일특가검색 인터페이스")
+	@RequestMapping(value="/service/callSddSearch")
+	public ResponseEntity<List<SddSearchOutSDO>> callSddSearch(UserAgentSDO userAgentDTO, HttpServletRequest request, HttpServletResponse response) {
+		logger.debug("[START] callView {}", userAgentDTO);
 		
-		ResponseEntity<ViewOutSDO> out = null;
-		ViewOutSDO serviceOut = null;
-
+		ResponseEntity<List<SddSearchOutSDO>> out = null;
+		List<SddSearchOutSDO> serviceOut = null;
+		SddSearchOutSDO sddSearchOut = null;
 		try {
-			if(viewInSDO == null) {
+			if(userAgentDTO == null) {
 				throw new APIException("입력값이 존재하지 않습니다.");
 			}
 			
 			//Advice & Interceptor 최적화후 작업 추가 진행
-			serviceOut = outsideService.callView(viewInSDO);
+			serviceOut = outsideService.callSddSearch(userAgentDTO);
 
-			out = new ResponseEntity<ViewOutSDO>(serviceOut, HttpStatus.OK);
+			out = new ResponseEntity<List<SddSearchOutSDO>>(serviceOut, HttpStatus.OK);
 		}
 		catch(Exception e) {
-			serviceOut = new ViewOutSDO(); 
-		
-			out = new ResponseEntity<ViewOutSDO>(serviceOut, HttpStatus.OK);
+			serviceOut = new ArrayList<SddSearchOutSDO>(); 
+			sddSearchOut = new SddSearchOutSDO();
+			
+			serviceOut.add(sddSearchOut);
+			
+			out = new ResponseEntity<List<SddSearchOutSDO>>(serviceOut, HttpStatus.OK);
 			e.printStackTrace();
 		}
 		
 		logger.debug("[END] callView {}", out);
 		return out;
 	}
-	
-	@ResponseBody
-	@APIOperation(description="시설바우처번호등록 인터페이스")
-	@RequestMapping(value="/{httpAgentId}/facl/voucherReg")
-	public ResponseEntity<VoucherRegOutSDO> callVoucherReg(@PathVariable("httpAgentId") String httpAgentId, VoucherRegInSDO voucherRegInSDO, HttpServletRequest request, HttpServletResponse response) {
-		logger.debug("[START] callVoucherReg {}", voucherRegInSDO);
-		
-		ResponseEntity<VoucherRegOutSDO> out = null;
-		VoucherRegOutSDO serviceOut = null;
-
-		try {
-			if(voucherRegInSDO == null) {
-				throw new APIException("입력값이 존재하지 않습니다.");
-			}
-			
-			//Advice & Interceptor 최적화후 작업 추가 진행
-			serviceOut = outsideService.callVoucherReg(voucherRegInSDO);
-
-			out = new ResponseEntity<VoucherRegOutSDO>(serviceOut, HttpStatus.OK);
-		}
-		catch(Exception e) {
-			serviceOut = new VoucherRegOutSDO(); 
-		
-			out = new ResponseEntity<VoucherRegOutSDO>(serviceOut, HttpStatus.OK);
-			e.printStackTrace();
-		}
-		
-		logger.debug("[END] callVoucherReg {}", out);
-		return out;
-	}
-	
-	
-	@ResponseBody
-	@APIOperation(description="주문대사(제휴사) 인터페이스")
-	@RequestMapping(value="/{httpAgentId}/order/agentJob")
-	public ResponseEntity<AgentJobOutSDO> callAgentJob(@PathVariable("httpAgentId") String httpAgentId, AgentJobInSDO agentJobInSDO, HttpServletRequest request, HttpServletResponse response) {
-		logger.debug("[START] callAgentJob {}", agentJobInSDO);
-		
-		ResponseEntity<AgentJobOutSDO> out = null;
-		AgentJobOutSDO serviceOut = null;
-
-		try {
-			if(agentJobInSDO == null) {
-				throw new APIException("입력값이 존재하지 않습니다.");
-			}
-			
-			//Advice & Interceptor 최적화후 작업 추가 진행
-			serviceOut = outsideService.callAgentJob(agentJobInSDO);
-
-			out = new ResponseEntity<AgentJobOutSDO>(serviceOut, HttpStatus.OK);
-		}
-		catch(Exception e) {
-			serviceOut = new AgentJobOutSDO(); 
-		
-			out = new ResponseEntity<AgentJobOutSDO>(serviceOut, HttpStatus.OK);
-			e.printStackTrace();
-		}
-		
-		logger.debug("[END] callAgentJob {}", out);
-		return out;
-	}	
 	
 }
