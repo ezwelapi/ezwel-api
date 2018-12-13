871927468b22b98a0689a7830538286598e38936
ezwelapiksw <ezwelapiksw@gmail.com>
2018-12-13 20:00:14 +0900

    ~

--- ezwel_project/ezwel_if_server/src/test/java/sample/InterfaceMixTestService.java
+++ ezwel_project/ezwel_if_server/src/test/java/sample/InterfaceMixTestService.java
@@ -6,18 +6,7 @@ import org.slf4j.LoggerFactory;
 import com.ezwel.htl.interfaces.adapter.OutsideIFAdapter;
 import com.ezwel.htl.interfaces.commons.configure.InterfaceFactory;
 import com.ezwel.htl.interfaces.commons.http.data.UserAgentSDO;
-import com.ezwel.htl.interfaces.service.data.cancelFeeAmt.CancelFeeAmtInSDO;
-import com.ezwel.htl.interfaces.service.data.cancelFeeAmt.CancelFeeAmtOutSDO;
-import com.ezwel.htl.interfaces.service.data.cancelFeePsrc.CancelFeePsrcInSDO;
-import com.ezwel.htl.interfaces.service.data.cancelFeePsrc.CancelFeePsrcOutSDO;
-import com.ezwel.htl.interfaces.service.data.ezwelJob.EzwelJobInSDO;
-import com.ezwel.htl.interfaces.service.data.ezwelJob.EzwelJobOutSDO;
-import com.ezwel.htl.interfaces.service.data.omiNumIdn.OmiNumIdnInSDO;
-import com.ezwel.htl.interfaces.service.data.omiNumIdn.OmiNumIdnOutSDO;
-import com.ezwel.htl.interfaces.service.data.orderCancelReq.OrderCancelReqInSDO;
-import com.ezwel.htl.interfaces.service.data.orderCancelReq.OrderCancelReqOutSDO;
-import com.ezwel.htl.interfaces.service.data.roomRead.RoomReadInSDO;
-import com.ezwel.htl.interfaces.service.data.roomRead.RoomReadOutSDO;
+import com.ezwel.htl.interfaces.service.OutsideIFService;
 import com.ezwel.htl.interfaces.service.data.rsvHistSend.RsvHistSendDataInSDO;
 import com.ezwel.htl.interfaces.service.data.rsvHistSend.RsvHistSendInSDO;
 import com.ezwel.htl.interfaces.service.data.rsvHistSend.RsvHistSendOutSDO;
@@ -26,22 +15,24 @@ import junit.framework.TestCase;
 
 /**
  * <pre>
- * 
+ * 테스트 도중 문제발생 케이스 디버깅용 테스트클래스
  * </pre>
  * @author swkim@ebsolution.co.kr
  * @date   2018. 11. 19.
  */
-public class BOInterfaceDemoTestService extends TestCase {
+public class InterfaceMixTestService extends TestCase {
 
-	private static final Logger logger = LoggerFactory.getLogger(BOInterfaceDemoTestService.class);
+	private static final Logger logger = LoggerFactory.getLogger(InterfaceMixTestService.class);
 	
-	private OutsideIFAdapter outIfAdapter; // if
+	private OutsideIFAdapter outIfAdapter; // if Adapter
+	
+	private OutsideIFService outIfService; // if Service
 	
 	/**
 	 * 아레 컨트스럭터는 로컬테스트 떄만 사용한다.
 	 * InterfaceFactory는 스프링 어플리케이션 초기화시 스프링 빈으로 초기화된다.
 	 */
-	public BOInterfaceDemoTestService()  throws Exception {
+	public InterfaceMixTestService()  throws Exception {
 		InterfaceFactory factory = new InterfaceFactory();
 		factory.setConfigXmlPath("/interfaces/interface-configure.xml");
 		factory.initFactory();
