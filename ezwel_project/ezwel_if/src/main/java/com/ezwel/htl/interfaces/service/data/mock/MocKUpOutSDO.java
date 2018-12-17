9624733d2916bce8de8742a1346195ca7c889411
ezwelapiksw <ezwelapiksw@gmail.com>
2018-12-17 11:13:44 +0900

    ~

--- ezwel_project/ezwel_if/src/main/java/com/ezwel/htl/interfaces/service/data/mock/MocKUpOutSDO.java
+++ ezwel_project/ezwel_if/src/main/java/com/ezwel/htl/interfaces/service/data/mock/MocKUpOutSDO.java
@@ -1,5 +1,15 @@
 package com.ezwel.htl.interfaces.service.data.mock;
 
+import com.ezwel.htl.interfaces.service.data.roomRead.RoomReadOutSDO;
+
 public class MocKUpOutSDO {
 
+	public static RoomReadOutSDO getRoomReadOut() {
+		RoomReadOutSDO out = new RoomReadOutSDO();
+		/** 데이터 세팅 */
+		
+		return out;
+	}
+	
+	
 }
