1c3390714b1025b6dc5b87442d370abc58189d27
java124 <java124@naver.com>
2018-12-18 18:10:50 +0900

    mockup 수정

--- ezwel_project/ezwel_if/src/main/java/com/ezwel/htl/interfaces/service/data/mock/MocKUpOutSDO.java
+++ ezwel_project/ezwel_if/src/main/java/com/ezwel/htl/interfaces/service/data/mock/MocKUpOutSDO.java
@@ -1,5 +1,8 @@
 package com.ezwel.htl.interfaces.service.data.mock;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.ezwel.htl.interfaces.service.data.cancelFeeAmt.CancelFeeAmtDataOutSDO;
 import com.ezwel.htl.interfaces.service.data.cancelFeeAmt.CancelFeeAmtOutSDO;
 import com.ezwel.htl.interfaces.service.data.cancelFeePsrc.CancelFeePsrcDataOutSDO;
@@ -21,20 +24,8 @@ public class MocKUpOutSDO {
 	//객실정보
 	public static RoomReadOutSDO getRoomReadOut() {
 		
-		RoomReadOutSDO roomReadOutSDO = new RoomReadOutSDO();
-		
 		//객실정보
 		RoomReadDataOutSDO roomReadDataOutSDO = new RoomReadDataOutSDO();
-		
-		//옵션정보
-		RoomReadOptionsOutSDO roomReadOptionsOutSDO = new RoomReadOptionsOutSDO();
-		
-		//취소규정정보
-		RoomReadPenaltyOutSDO roomReadPenaltyOutSDO = new RoomReadPenaltyOutSDO();
-		
-		/** 데이터 세팅 */        
-		roomReadOutSDO.setCode("1000");
-		roomReadOutSDO.setMessage("정상적으로 처리 되었습니다.");
 		roomReadDataOutSDO.setRoomNo("Yob1daQcPbOlVncEHz/Q62kMjcnsyRrO0QH/U/qJzoi^JE^t1pCAhgNe80PkfWL7seEbLG22^1yF65eQCg9UALt1KK7eTF/XQ^Q53nLhuVGd0lTKdaS9IK/PECb37ksVU9o4QzaTuwBV6lz3IIxb/BBSKZioaVq/uQPIpaCiz6rbRGGFFBalAziFGnZP2g^YYjZPp4cfKWn0dW^iVtNuwe0GuSwsRYYTuBFKnY/5abk=");
 		roomReadDataOutSDO.setRoomName("Standard Double Room(1 double bed request)");
 		roomReadDataOutSDO.setPriceForList(58000);
@@ -47,6 +38,9 @@ public class MocKUpOutSDO {
 		roomReadDataOutSDO.setBreakfast("0");
 		roomReadDataOutSDO.setSpcType("0");
 		roomReadDataOutSDO.setSpcTypeTime("");
+		
+		//옵션정보
+		RoomReadOptionsOutSDO roomReadOptionsOutSDO = new RoomReadOptionsOutSDO();
 		roomReadOptionsOutSDO.setOptNo("1");
 		roomReadOptionsOutSDO.setOptName("조식");
 		roomReadOptionsOutSDO.setOptPrice(28000);
@@ -55,6 +49,9 @@ public class MocKUpOutSDO {
 		roomReadOptionsOutSDO.setOptName("Extra bed");
 		roomReadOptionsOutSDO.setOptPrice(15000);
 		roomReadOptionsOutSDO.setOptCountMax(1);
+		
+		//취소규정정보
+		RoomReadPenaltyOutSDO roomReadPenaltyOutSDO = new RoomReadPenaltyOutSDO();		
 		roomReadPenaltyOutSDO.setApplyBgnDate("20181229");
 		roomReadPenaltyOutSDO.setCancelFeeRate(100);
 		roomReadPenaltyOutSDO.setCancelFeePrice(58000);
@@ -66,24 +63,36 @@ public class MocKUpOutSDO {
 		roomReadPenaltyOutSDO.setCancelFeePrice(58000);
 		roomReadPenaltyOutSDO.setApplyBgnDate("20190101");
 		roomReadPenaltyOutSDO.setCancelFeeRate(100);
-		roomReadPenaltyOutSDO.setCancelFeePrice(58000);		
+		roomReadPenaltyOutSDO.setCancelFeePrice(58000);
 		
-		return roomReadOutSDO;
+		List<RoomReadOptionsOutSDO> optionList = new ArrayList<>();
+		optionList.add(roomReadOptionsOutSDO);
+		roomReadDataOutSDO.setOptions(optionList);
+		
+		List<RoomReadPenaltyOutSDO> penaltyList = new ArrayList<>();
+		penaltyList.add(roomReadPenaltyOutSDO);
+		roomReadDataOutSDO.setPenalty(penaltyList);
+		
+		List<RoomReadDataOutSDO> dataList = new ArrayList<>();
+		dataList.add(roomReadDataOutSDO);
+		
+		RoomReadOutSDO out = new RoomReadOutSDO();
+		out.setCode("1000");
+		out.setMessage("정상적으로 처리 되었습니다.");
+		out.setData(dataList);
+		
+		return out;
 	}
 	
 	//취소규정정보
 	public static CancelFeePsrcOutSDO getCancelFeePsrcOut() {
 		
-		CancelFeePsrcOutSDO cancelFeePsrcOutSDO = new CancelFeePsrcOutSDO();
-		
 		//취소수수료 대체텍스트
 		CancelFeePsrcDataOutSDO cancelFeePsrcDataOutSDO = new CancelFeePsrcDataOutSDO();
 		
 		//취소수수료정보
-		CancelFeePsrcPenaltyOutSDO cancelFeePsrcPenaltyOutSDO = new CancelFeePsrcPenaltyOutSDO();
+		CancelFeePsrcPenaltyOutSDO cancelFeePsrcPenaltyOutSDO = new CancelFeePsrcPenaltyOutSDO();		
 		
-		cancelFeePsrcOutSDO.setCode("1000");
-		cancelFeePsrcOutSDO.setMessage("정상적으로 처리 되었습니다.");
 		cancelFeePsrcDataOutSDO.setCancelFeeText("17:00 이후 익일 취소규정 적용 되어집니다.");
 		cancelFeePsrcPenaltyOutSDO.setApplyBgnDate("20181229");
 		cancelFeePsrcPenaltyOutSDO.setCancelFeeRate(100);
@@ -96,16 +105,28 @@ public class MocKUpOutSDO {
 		cancelFeePsrcPenaltyOutSDO.setCancelFeePrice(58000);
 		cancelFeePsrcPenaltyOutSDO.setApplyBgnDate("20190101");
 		cancelFeePsrcPenaltyOutSDO.setCancelFeeRate(100);
-		cancelFeePsrcPenaltyOutSDO.setCancelFeePrice(58000);		
+		cancelFeePsrcPenaltyOutSDO.setCancelFeePrice(58000);
 		
-		return cancelFeePsrcOutSDO;
+		//취소수수료정보선언
+		List<CancelFeePsrcPenaltyOutSDO> penaltyList = new ArrayList<>();		
+		penaltyList.add(cancelFeePsrcPenaltyOutSDO);
+		
+		cancelFeePsrcDataOutSDO.setPenalty(penaltyList);
+		
+		//취소수수료전문
+		CancelFeePsrcOutSDO out = new CancelFeePsrcOutSDO();
+		out.setCode("1000");
+		out.setMessage("정상적으로 처리 되었습니다.");
+		out.setData(cancelFeePsrcDataOutSDO);
+		
+		return out;
 	}
 	
 	//결재완료내역전송
 	public static RsvHistSendOutSDO getRsvHistSendOut() {
-			
-		RsvHistSendOutSDO rsvHistSendOutSDO = new RsvHistSendOutSDO();
 		
+		//결제완료내역전문
+		RsvHistSendOutSDO rsvHistSendOutSDO = new RsvHistSendOutSDO();		
 		rsvHistSendOutSDO.setCode("1000");
 		rsvHistSendOutSDO.setMessage("정상적으로 처리 되었습니다.");
 		rsvHistSendOutSDO.setRsvNo("E123456789");
@@ -116,59 +137,60 @@ public class MocKUpOutSDO {
 	
 	//취소수수료계산
 	public static CancelFeeAmtOutSDO getCancelFeeAmtOut() {
-				
-		CancelFeeAmtOutSDO cancelFeeAmtOutSDO = new CancelFeeAmtOutSDO();
 		
 		//취소위약금
 		CancelFeeAmtDataOutSDO cancelFeeAmtDataOutSDO = new CancelFeeAmtDataOutSDO();
 		
-		cancelFeeAmtOutSDO.setCode("1000");
-		cancelFeeAmtOutSDO.setMessage("정상적으로 처리 되었습니다.");
 		cancelFeeAmtDataOutSDO.setRsvNo("E123456789");
 		cancelFeeAmtDataOutSDO.setCancelFeePrice(0);
 		cancelFeeAmtDataOutSDO.setCancelFeeText("17:00 이후 익일 취소규정 적용 되어집니다.");
-			
-		return cancelFeeAmtOutSDO;
+		
+		//취소수수료계산전문
+		CancelFeeAmtOutSDO out = new CancelFeeAmtOutSDO();
+		out.setCode("1000");
+		out.setMessage("정상적으로 처리 되었습니다.");
+		out.setData(cancelFeeAmtDataOutSDO);
+		
+		return out;
 	}
 	
 	//주문취소요청
 	public static OrderCancelReqOutSDO getOrderCancelReqOut() {
 		
-		OrderCancelReqOutSDO orderCancelReqOutSDO = new OrderCancelReqOutSDO();
+		//주문취소전문
+		OrderCancelReqOutSDO out = new OrderCancelReqOutSDO();		
+		out.setCode("1000");
+		out.setMessage("정상적으로 처리 되었습니다.");
 		
-		orderCancelReqOutSDO.setCode("1000");
-		orderCancelReqOutSDO.setMessage("정상적으로 처리 되었습니다.");
-		
-		return orderCancelReqOutSDO;
+		return out;
 	}
 	
 	//누락건확인
 	public static OmiNumIdnOutSDO getOmiNumIdnOut() {
 		
-		OmiNumIdnOutSDO omiNumIdnOutSDO = new OmiNumIdnOutSDO();
-		
 		//예약정보
 		OmiNumIdnReservesOutSDO omiNumIdnReservesOutSDO = new OmiNumIdnReservesOutSDO();
 		
-		omiNumIdnOutSDO.setCode("1000");
-		omiNumIdnOutSDO.setMessage("정상적으로 처리 되었습니다.");
 		omiNumIdnReservesOutSDO.setRsvNo("E123456789");
 		omiNumIdnReservesOutSDO.setRsvStat("r02");
 		omiNumIdnReservesOutSDO.setOtaRsvNo("O123456789");
 		omiNumIdnReservesOutSDO.setCompareStat("c01");
 		
-		return omiNumIdnOutSDO;
+		//누락건확인전문
+		OmiNumIdnOutSDO out = new OmiNumIdnOutSDO();		
+		out.setCode("1000");
+		out.setMessage("정상적으로 처리 되었습니다.");
+		out.setReserves(omiNumIdnReservesOutSDO);
+		
+		return out;
 	}
 	
 	//주문대사(이지웰)
 	public static EzwelJobOutSDO getEzwelJobOut() {
 		
-		EzwelJobOutSDO ezwelJobOutSDO = new EzwelJobOutSDO();
-		
+		//예약정보
 		EzwelJobReservesOutSDO ezwelJobReservesOutSDO = new EzwelJobReservesOutSDO();
 		
-		ezwelJobOutSDO.setCode("1000");
-		ezwelJobOutSDO.setMessage("정상적으로 처리 되었습니다.");
 		ezwelJobReservesOutSDO.setRsvNo("E123456789");
 		ezwelJobReservesOutSDO.setRsvPdtNo("E000000001");
 		ezwelJobReservesOutSDO.setRsvPrice(58000);
@@ -176,7 +198,17 @@ public class MocKUpOutSDO {
 		ezwelJobReservesOutSDO.setOtaRsvNo("O123456789");
 		ezwelJobReservesOutSDO.setRsvStat("r02");
 		
-		return ezwelJobOutSDO;
+		//대사정보선언
+		List<EzwelJobReservesOutSDO> reservesList = new ArrayList<>();
+		reservesList.add(ezwelJobReservesOutSDO);
+		
+		//주문대사전문
+		EzwelJobOutSDO out = new EzwelJobOutSDO();		
+		out.setCode("1000");
+		out.setMessage("정상적으로 처리 되었습니다.");
+		out.setReserves(reservesList);
+		
+		return out;
 		
 	}
 	
