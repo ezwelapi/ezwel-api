e85b107c71a77f07b26f4b304a0b169d4d497d77
ezwelapiksw <ezwelapiksw@gmail.com>
2018-12-28 10:42:45 +0900

    ~

--- ezwel_project/ezwel_if/src/main/java/com/ezwel/htl/interfaces/commons/utils/CryptUtil.java
+++ ezwel_project/ezwel_if/src/main/java/com/ezwel/htl/interfaces/commons/utils/CryptUtil.java
@@ -1,5 +1,6 @@
 package com.ezwel.htl.interfaces.commons.utils;
 
+import org.apache.commons.codec.digest.DigestUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,25 +14,63 @@ public class CryptUtil {
 	private static final Logger logger = LoggerFactory.getLogger(CryptUtil.class);
 
 	public static String getEncodeBase64(String word) {
+		if(APIUtil.isEmpty(word)) {
+			logger.warn("EncodeBase64 input parameter is empty or null...");
+			return word;
+		}
+		
 		String out = Base64Codec.getInstance().encode(word);
 		return out;
 	}
 	
 	public static String getDecodeBase64(String word) {
+		if(APIUtil.isEmpty(word)) {
+			logger.warn("DecodeBase64 input parameter is empty or null...");
+			return word;
+		}
+		
 		String out = Base64Codec.getInstance().decode(word);
 		return out;
 	}
 	
 	public static String getMD5HashString(String word) {
+		if(APIUtil.isEmpty(word)) {
+			logger.warn("MD5HashString input parameter is empty or null...");
+			return word;
+		}
+		
 		String out = MD5.getInstance().getHashString(word);
 		return out;
 	}
 	
 	public static byte[] getMD5Hash(String word) {
+		if(APIUtil.isEmpty(word)) {
+			logger.warn("MD5Hash input parameter is empty or null...");
+			return null;
+		}
+		
 		byte[] out = MD5.getInstance().getHash(word);
 		return out;
 	}
 	
+	public static byte[] getSHA256(String word) {
+		if(APIUtil.isEmpty(word)) {
+			logger.warn("SHA256 input parameter is empty or null...");
+			return null;
+		}
+		
+		byte[] out = DigestUtils.sha256(word);
+		return out;
+	}
 	
+	public static String getSHA256Hex(String word) {
+		if(APIUtil.isEmpty(word)) {
+			logger.warn("SHA256Hex input parameter is empty or null...");
+			return null;
+		}
+		
+		String out = DigestUtils.sha256Hex(word);
+		return out;
+	}
 	
 }
