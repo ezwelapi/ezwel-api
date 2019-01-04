ba090197fbf960cf21859069af5c8834cb669ccc
java124 <java124@naver.com>
2019-01-04 11:41:50 +0900

    1

--- ezwel_project/ezwel_if/src/test/java/interfaces/example/InterfaceMixTestService.java
+++ ezwel_project/ezwel_if/src/test/java/interfaces/example/InterfaceMixTestService.java
@@ -6,10 +6,9 @@ import org.slf4j.LoggerFactory;
 import com.ezwel.htl.interfaces.adapter.OutsideIFAdapter;
 import com.ezwel.htl.interfaces.commons.configure.InterfaceFactory;
 import com.ezwel.htl.interfaces.commons.http.data.UserAgentSDO;
-import com.ezwel.htl.interfaces.service.EzcsendIFService;
 import com.ezwel.htl.interfaces.service.OutsideIFService;
-import com.ezwel.htl.interfaces.service.data.send.SendSmsInSDO;
-import com.ezwel.htl.interfaces.service.data.send.SendSmsOutSDO;
+import com.ezwel.htl.interfaces.service.data.roomRead.RoomReadInSDO;
+import com.ezwel.htl.interfaces.service.data.roomRead.RoomReadOutSDO;
 
 import junit.framework.TestCase;
 
@@ -28,8 +27,6 @@ public class InterfaceMixTestService extends TestCase {
 	
 	private OutsideIFService outIfService; // if Service
 	
-	private EzcsendIFService ezcIfService; // if Service
-	
 	/**
 	 * 아레 컨트스럭터는 로컬테스트 떄만 사용한다.
 	 * InterfaceFactory는 스프링 어플리케이션 초기화시 스프링 빈으로 초기화된다.
@@ -39,35 +36,37 @@ public class InterfaceMixTestService extends TestCase {
 		
 		outIfAdapter = new OutsideIFAdapter();
 		outIfService = new OutsideIFService();
-		ezcIfService = new EzcsendIFService();
 	}
 
-	public void testSendSms()  throws Exception {
-		logger.debug("[START] testSendSms");
+	public void testRoomRead()  throws Exception {
+		logger.debug("[START] callRoomRead");
 		
 		UserAgentSDO userAgentDTO = new UserAgentSDO();
 		
-		userAgentDTO.setHttpAgentId("99999999"); //직영숙박
+		userAgentDTO.setHttpAgentId("10055550"); //호텔패스
+		userAgentDTO.setHttpAgentType("AP02PO");
+		userAgentDTO.setHttpChannelCd("1");
+		userAgentDTO.setHttpClientId("ez1");
+		userAgentDTO.setHttpRequestId("test");
 		
 		//Input parameter
-		SendSmsInSDO sendSmsSDO = new SendSmsInSDO();
+		RoomReadInSDO roomReadSDO = new RoomReadInSDO();
 		
-		sendSmsSDO.setCallTo("01037440698");
-		sendSmsSDO.setCallFrom("0232820579");
-		sendSmsSDO.setMmsSubject("테스트");
-		sendSmsSDO.setSmsText("테스트");
-		sendSmsSDO.setSvcType("1008");
-		sendSmsSDO.setSmsUseYn("N");
-		sendSmsSDO.setTemplateCode("N");
+		roomReadSDO.setPdtNo("KRSEL217");
+		roomReadSDO.setCheckInDate("20190114");
+		roomReadSDO.setCheckOutDate("20190115");
+		roomReadSDO.setRoomCnt(1);
+		roomReadSDO.setAdultCnt(2);
+		roomReadSDO.setChildCnt(0);
 		
 		//interface api call
-		SendSmsOutSDO out = ezcIfService.callSendSms(userAgentDTO, sendSmsSDO);
+		RoomReadOutSDO out = outIfService.callRoomRead(userAgentDTO, roomReadSDO);
 		
-		logger.debug("errorCode : {}", out.getErrorCode());
-		logger.debug("errorMessage : {}", out.getErrorMessage());
-		logger.debug("data : {}", out.getData());
+		logger.debug("Code : {}", out.getCode());
+		logger.debug("Message : {}", out.getMessage());
+		logger.debug("Data : {}", out.getData());
 		
-		logger.debug("[END] testSendSms");
+		logger.debug("[END] callRoomRead");
 	}
 	
 }
